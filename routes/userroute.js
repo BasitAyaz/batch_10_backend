b429d48d2263f85389cb16c617903b7b60841521
BasitAyaz <basitahmed1997@gmail.com>
2024-09-07 14:49:13 +0500

    Middleware Work

--- routes/userroute.js
+++ routes/userroute.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const UserController = require('../controller/usercontroller');
+const express = require("express");
+const UserController = require("../controller/usercontroller");
+const AuthController = require("../controller/authcontroller");
 const Route = express.Router();
 
-Route.get('/', UserController.getAll)
-Route.get('/:id', UserController.getById)
-Route.post('/', UserController.add)
-Route.put('/:id', UserController.update)
-Route.delete('/:id', UserController.del)
+Route.get("/", AuthController.protected, UserController.getAll);
+Route.get("/:id", UserController.getById);
+Route.post("/", UserController.add);
+Route.put("/:id", UserController.update);
+Route.delete("/:id", UserController.del);
 
 module.exports = Route;
